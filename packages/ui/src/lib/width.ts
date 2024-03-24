7a7265b0c694b2a6c06a78459c12a9ac64cf4ee6
samarmeena <sumarmeena@gmail.com>
2024-03-24 17:52:17 +0530

    chore: bump version

--- packages/ui/src/lib/width.ts
+++ packages/ui/src/lib/width.ts
@@ -1,5 +1,11 @@
 "use client";
 
+/*
+ * --------------------------------------------------------------------------------------------------------
+ * Copyright (c) Vijay Meena <vijayymmeena@gmail.com> (https://github.com/samarmeena). All rights reserved.
+ * Licensed under the Apache License. See License.txt in the project root for license information.
+ * --------------------------------------------------------------------------------------------------------
+ */
 import { useState, useEffect } from "react";
 
 const useWidthPercentage = (targetWidth: number, defaultPercent: number) => {
