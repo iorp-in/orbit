1acdc105a3ea5be39c1925730b3611d647a2595e
samarmeena <sumarmeena@gmail.com>
2024-03-25 10:27:38 +0530

    refactor: sorting

--- packages/ui/src/components/molecules/server-list.tsx
+++ packages/ui/src/components/molecules/server-list.tsx
@@ -4,107 +4,215 @@
  * Licensed under the Apache License. See License.txt in the project root for license information.
  * --------------------------------------------------------------------------------------------------------
  */
-import { ScrollArea, ScrollBar } from "../ui/scroll-area";
 import { serverIndexAtom } from "@/atoms/server";
 import { serversFilteredAtom } from "@/atoms/server/servers";
+import { Button } from "@/components/ui/button";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import api from "@/lib/api";
 import { cn } from "@/lib/utils";
 import { ServerInfo } from "@/types/server-info";
-import { LockClosedIcon, LockOpen1Icon } from "@radix-ui/react-icons";
+import {
+  CaretSortIcon,
+  LockClosedIcon,
+  LockOpen1Icon,
+} from "@radix-ui/react-icons";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 import { useAtom, useAtomValue } from "jotai";
-import React from "react";
+import * as React from "react";
 
-const ServerRow = ({
-  server,
-  index,
-}: {
-  server: ServerInfo;
-  index: number;
-}) => {
-  const [selected, setSelected] = useAtom(serverIndexAtom);
-
-  const isSelected = selected === index;
-  const {
-    address,
-    hostname,
-    online,
-    maxplayers,
-    gamemode,
-    mapname,
-    ping,
-    passworded = false,
-  } = server;
-
-  return (
-    <TableRow
-      onClick={() => {
-        void api?.invoke("server-info", server, true);
-        setSelected(index);
-      }}
-      className={cn({
-        "bg-muted": isSelected,
-        "text-muted-foreground": !isSelected,
-      })}
-    >
-      <TableCell>
-        {passworded ? (
-          <LockClosedIcon className="h-4 w-4 text-red-400" />
-        ) : (
-          <LockOpen1Icon className="h-4 w-4 text-green-400" />
-        )}
-      </TableCell>
-      <TableCell>{hostname ?? address}</TableCell>
-      <TableCell>
-        {online !== undefined && maxplayers !== undefined
-          ? `${String(online)}/${String(maxplayers)}`
-          : "-"}
-      </TableCell>
-      <TableCell>{ping ?? "-"}</TableCell>
-      <TableCell>{gamemode ?? "-"}</TableCell>
-      <TableCell>{mapname ?? "-"}</TableCell>
-    </TableRow>
-  );
-};
+const columns: ColumnDef<ServerInfo>[] = [
+  {
+    id: "password",
+    header: "",
+    accessorFn: (data) => data.passworded ?? false,
+    cell: ({ cell }) =>
+      cell.getValue() ? (
+        <LockClosedIcon className="h-4 w-4 text-red-400" />
+      ) : (
+        <LockOpen1Icon className="h-4 w-4 text-green-400" />
+      ),
+  },
+  {
+    id: "hostname",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => {
+          column.toggleSorting(column.getIsSorted() === "asc");
+        }}
+      >
+        HostName
+        <CaretSortIcon className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    accessorFn: (data) => data.hostname ?? data.address,
+  },
+  {
+    id: "players",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => {
+          column.toggleSorting(column.getIsSorted() === "asc");
+        }}
+      >
+        Players
+        <CaretSortIcon className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    accessorFn: ({ online, maxplayers }) =>
+      online !== undefined && maxplayers !== undefined
+        ? `${String(online)}/${String(maxplayers)}`
+        : "-",
+  },
+  {
+    id: "ping",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => {
+          column.toggleSorting(column.getIsSorted() === "asc");
+        }}
+      >
+        Ping
+        <CaretSortIcon className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    accessorFn: (data) => data.ping ?? "-",
+    enableSorting: true,
+  },
+  {
+    id: "mode",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => {
+          column.toggleSorting(column.getIsSorted() === "asc");
+        }}
+      >
+        Mode
+        <CaretSortIcon className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    accessorFn: (data) => data.gamemode ?? "-",
+  },
+  {
+    id: "language",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => {
+          column.toggleSorting(column.getIsSorted() === "asc");
+        }}
+      >
+        Language
+        <CaretSortIcon className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    accessorFn: (data) => data.mapname ?? "-",
+  },
+];
 
 export default function ServerList() {
   const servers = useAtomValue(serversFilteredAtom);
+  const [serverIndex, setServerIndex] = useAtom(serverIndexAtom);
 
-  const renderServers = () => {
-    return servers.map((server, index) => (
-      <ServerRow
-        key={`server-${String(index)}`}
-        server={server}
-        index={index}
-      />
-    ));
-  };
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    [],
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+
+  const table = useReactTable({
+    data: servers,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
 
   return (
     <ScrollArea className="h-[calc(100vh-88px)]">
       <ScrollBar orientation="vertical" />
       <ScrollBar orientation="horizontal" />
       <Table className="whitespace-nowrap text-xs">
-        {servers.length === 0 && <TableCaption>No servers</TableCaption>}
         <TableHeader>
-          <TableRow>
-            <TableHead></TableHead>
-            <TableHead>HostName</TableHead>
-            <TableHead>Players</TableHead>
-            <TableHead>Ping</TableHead>
-            <TableHead>Mode</TableHead>
-            <TableHead>Language</TableHead>
-          </TableRow>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
         </TableHeader>
-        <TableBody>{renderServers()}</TableBody>
+        <TableBody>
+          {table.getRowModel().rows.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+                className={cn(
+                  serverIndex === row.index
+                    ? "bg-muted"
+                    : "text-muted-foreground",
+                )}
+                onClick={() => {
+                  setServerIndex(row.index);
+                }}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
       </Table>
     </ScrollArea>
   );
